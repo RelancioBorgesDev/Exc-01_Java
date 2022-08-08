3705e742f1b3296bb3a3bb7e5ec43f053226b1c1
Relancio Borges <relancio.dev@gmail.com>
2022-08-08 15:38:43 -0300

    Exercicios 01 - Curso Nelio Alves

--- src/Exc02/Main.java
+++ src/Exc02/Main.java
@@ -1,9 +1,11 @@
 package Exc02;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         double pi = 3.14159;
         double raio = sc.nextDouble();
